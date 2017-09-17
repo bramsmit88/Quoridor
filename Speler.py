54ebe44a418a5b50a59ea4ec003988a5e4739eee
unknown <bramsmit88@gmail.com>
2017-09-17 16:43:33 +0200

    bewegen doet het, alleen nog restrictie van schotjes in te bouwen (daarom is schuin nog toegestaan)

--- Speler.py
+++ Speler.py
@@ -8,17 +8,18 @@ import pygame
 import numpy as np
 
 class Speler:
-    def __init__(self,x,y,spel_config,kleur):
+    def __init__(self,x,y,spel_config,kleur,r):
         self.x = x
         self.y = y
         self.spel_config = spel_config
         self.kleur  = kleur
+        self.r = r
+        self.keydownevents = 0
         
     def drawme(self,gameDisplay):
         kleur = self.kleur
-        r = 10
         x_pixel, y_pixel = self.grid2pos(self.x,self.y,self.spel_config)
-        pygame.draw.circle(gameDisplay,kleur,(np.int(x_pixel),np.int(y_pixel)),r)
+        pygame.draw.circle(gameDisplay,kleur,(np.int(x_pixel),np.int(y_pixel)),self.r)
     
     def grid2pos(self,x_grid,y_grid,spel_config):
         blokgrootte = spel_config['blokgrootte']
@@ -28,4 +29,22 @@ class Speler:
         x_pos = dx + (x_grid-.5)*blokgrootte + (x_grid-1)*lijndikte
         y_pos = dy + (y_grid-.5)*blokgrootte + (y_grid-1)*lijndikte
         return x_pos, y_pos
-        
+    
+    def move(self,event):
+        finish = 0
+        # detecteer keypress
+        if event.type == pygame.KEYDOWN:
+            self.keydownevents = self.keydownevents + 1
+            if (event.key == pygame.K_UP and self.keydownevents == 1 and self.y > 1):
+                self.y = self.y - 1
+            elif (event.key == pygame.K_DOWN and self.keydownevents == 1 and self.y <9):
+                self.y = self.y + 1
+            elif (event.key == pygame.K_LEFT and self.keydownevents == 1 and self.x > 1):
+                self.x = self.x - 1
+            elif (event.key == pygame.K_RIGHT and self.keydownevents == 1 and self.x <9):
+                self.x = self.x + 1
+            elif event.key == pygame.K_SPACE and self.keydownevents == 1:
+                finish = 1
+        elif event.type == pygame.KEYUP:
+            self.keydownevents = 0
+        return finish
--- game_main.py
+++ game_main.py
@@ -20,22 +20,43 @@ def drawall():
             pygame.draw.rect(gameDisplay, kleur_achtergrond, rect)            
     # pionnen
     Speler1.drawme(gameDisplay)
+    select1.drawme(gameDisplay)
     Speler2.drawme(gameDisplay)
-    #bloktest.drawme(gameDisplay)
-    for i in range(len(schottestrij)):
-        (schottestrij[i]).drawme(gameDisplay)
+    select2.drawme(gameDisplay)
+    for i in range(len(schotjes)):
+        (schotjes[i]).drawme(gameDisplay)
 
-
-
-
-## initialisatie
-# pygame settings:
-pygame.init()
-Display_width   = 800
-Display_height  = 600
-gameDisplay     = pygame.display.set_mode((Display_width,Display_height))
-pygame.display.set_caption('Quoridor')
-clock           = pygame.time.Clock()
+def checkmove(speler_id):
+    if speler_id == 1:
+        speler      = Speler1
+        selector    = select1
+        tegenspeler = Speler2
+    else:
+        speler      = Speler2
+        selector    = select2
+        tegenspeler = Speler1
+    
+    possiblemove = 0
+    if not ((selector.x == speler.x and selector.y == speler.y) or (selector.x == tegenspeler.x and selector.y == tegenspeler.y) ):
+        if abs(speler.x - selector.x) == 1:
+            if abs(speler.y - selector.y) == 0:
+                possiblemove = 1
+            elif abs(speler.y - selector.y) == 1:
+                # mag alleen als er naast de tegenspeler een verticaal schotje staat
+                possiblemove = 1 # AANPASSEN!
+        elif abs(speler.x - selector.x) == 2:
+            if speler.x + 0.5*(selector.x-speler.x) == tegenspeler.x and abs(selector.y-speler.y)==0:
+                possiblemove = 1
+        if abs(speler.y - selector.y) == 1:
+            if abs(speler.x - selector.x) == 0:
+                possiblemove = 1
+            elif abs(speler.x - selector.x) == 1:
+                # mag alleen als er onder/boven de tegenspeler een horizontaal schotje staat
+                possiblemove = 1 # AANPASSEN!   
+        elif abs(speler.y - selector.y) == 2:
+            if speler.y + 0.5*(selector.y-speler.y) == tegenspeler.y and abs(selector.x-speler.x)==0:
+                possiblemove = 1
+    return possiblemove
 
 # spel configuratie
 kleur_achtergrond   = (200,200,200)
@@ -44,25 +65,68 @@ kleur_speler1       = (0,100,0)
 kleur_speler2       = (100,1,100)
 kleur_select        = (255,255,0)
 spel_config = {'blokgrootte': 50, 'lijndikte':10, 'dx': 130, 'dy':40}
-Speler1 = Speler.Speler(5,1,spel_config,kleur_speler1)
-Speler2 = Speler.Speler(5,9,spel_config,kleur_speler2)
+Speler1 = Speler.Speler(5,1,spel_config,kleur_speler1,10)
+select1 = Speler.Speler(Speler1.x,Speler1.y,spel_config,kleur_select,0)
+Speler2 = Speler.Speler(5,9,spel_config,kleur_speler2,10)
+select2 = Speler.Speler(Speler2.x,Speler2.y,spel_config,kleur_select,0)
+schotjes =[]
 
-schottestrij =[]
-schottestrij.append(schotje.schotje(1,3,'h',spel_config))
-schottestrij.append(schotje.schotje(3,3,'h',spel_config))
-#
-quitrequested = 0
-while not quitrequested:
+#schotjes.append(schotje.schotje(1,3,'h',spel_config))
+#schotjes.append(schotje.schotje(3,3,'h',spel_config))
+
+#MAIN 
+# pygame settings:
+pygame.init()
+Display_width   = 800
+Display_height  = 600
+gameDisplay     = pygame.display.set_mode((Display_width,Display_height))
+pygame.display.set_caption('Quoridor')
+clock           = pygame.time.Clock()
+#$ MAIN LOOP
+gamemode = 1
+keydownevents = 0
+done = False
+while not done:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: #als op kruisje wordt gedrukt
-            quitrequested = True
-   
-    #main game loop:    
-    drawall()
+            if event.type == pygame.QUIT:
+                    done = True
+    if gamemode == 1:
+        select1 = Speler.Speler(Speler1.x,Speler1.y,spel_config,kleur_select,5)
+        gamemode = 2
     
-    
-    pygame.display.update()
-    clock.tick(50) #in fps
+    elif gamemode == 2:
+        finish = select1.move(event)
+        if finish == 1:
+            possiblemove = checkmove(1)
+            if possiblemove:                
+                #verplaats
+                Speler1 = Speler.Speler(select1.x,select1.y,spel_config,kleur_speler1,10)
+                select1 = Speler.Speler(Speler1.x,Speler1.y,spel_config,kleur_select,0)
+                gamemode = 3
+            else:
+                finish = 0
+    elif gamemode == 3:
+        select2 = Speler.Speler(Speler2.x,Speler2.y,spel_config,kleur_select,5)
+        gamemode = 4    
+
+    elif gamemode == 4:    
+        finish = select2.move(event)
+        if finish == 1:
+            possiblemove = checkmove(2)
+            if possiblemove:
+                Speler2 = Speler.Speler(select2.x,select2.y,spel_config,kleur_speler2,10)
+                select2 = Speler.Speler(Speler2.x,Speler2.y,spel_config,kleur_select,0)    
+                gamemode = 1
+            else:
+                finish = 0
+        
+
+
+            
+                
+                
+
+    drawall()    
+    pygame.display.flip()
+    clock.tick(60)
 pygame.quit()
-
-
